ec492adc543e075c9fd95fe92e6d49bc675bc212
shane <953762287@qq.com>
2021-02-07 23:02:07 +0800

    add_day11

---
+++ Week_02/Day11 412. Fizz Buzz.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        """
+        取余
+        """
+        r = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                r.append("FizzBuzz")
+            elif i % 3 == 0:
+                r.append("Fizz")
+            elif i % 5 == 0:
+                r.append("Buzz")
+            else:
+                r.append(str(i))
+        
+        return r
