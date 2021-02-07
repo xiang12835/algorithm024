94fed22a34755100db71e556c13467e088c6c304
shane <953762287@qq.com>
2021-02-07 18:41:37 +0800

    upd_347

--- Week_02/347. Top K Frequent Elements.py
+++ Week_02/347. Top K Frequent Elements.py
@@ -13,4 +13,24 @@ class Solution:
         nums = list(set(nums))
         nums.sort(key=lambda n:d[n], reverse=True)
         return nums[:k]
-        
+
+class Solution1:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+
+        法二：去构建一个大顶堆，然后去删除k次，取k个最大值
+
+        T: O(nlogn)
+        S: O(n)
+        """
+        import collections, heapq
+
+        d = collections.Counter(nums)
+        hp, ans = [], []
+        for i in d:
+            heapq.heappush(hp, (-d[i], i))
+        for _ in range(k):
+            ans.append(heapq.heappop(hp)[1])  # 每删除一个都需要调整堆
+        return ans
+
+
