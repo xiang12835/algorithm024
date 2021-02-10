734de0579cfccb8976c59c5596fec7c22627b4b9
shane <953762287@qq.com>
2021-02-10 18:13:01 +0800

    upd_22

--- Week_03/22. Generate Parentheses.py
+++ Week_03/22. Generate Parentheses.py
@@ -7,10 +7,10 @@ class Solution:
         T: O(2^n)
         """
         self.l = []
-        self._gen(0, 0, n, "")
+        self._recursion(0, 0, n, "")
         return self.l
 
-    def _gen(self, left, right, n, result):
+    def _recursion(self, left, right, n, result):
         # terminator
         if left == n and right == n:
             self.l.append(result)
@@ -18,9 +18,9 @@ class Solution:
         # process current logic: left, right
         # drill down
         if left < n:
-            self._gen(left+1, right, n, result+"(")
+            self._recursion(left+1, right, n, result+"(")
         
         if right < n and right < left:
-            self._gen(left, right+1, n, result+")")
+            self._recursion(left, right+1, n, result+")")
 
         # reverse states
