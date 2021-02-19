4cb88df5c23a9e7ecd168e3a9bc09fc21c99e160
shane <953762287@qq.com>
2021-02-19 18:56:54 +0800

    add_69

---
+++ Week_04/69. Sqrt(x).py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        """
+        二分查找
+
+        T: O(x)
+        S: O(1)
+        """
+        l = 1
+        r = x
+        while l <= r:
+            mid = l + (r - l) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return r # 当无整数平方根时，取小值
+        
