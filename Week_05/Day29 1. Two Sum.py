3c1052678e661224d9f910d6e1658e1e3a6c14b5
shane <953762287@qq.com>
2021-02-27 20:49:03 +0800

    day29

---
+++ Week_05/Day29 1. Two Sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+        哈希表
+        T: O(n)
+        S: O(n)
+        """
+        d = {}
+        for i, v in enumerate(nums):
+            if target - v in d:
+                return [d[target - v], i]
+            else:
+                d[v] = i
+                
