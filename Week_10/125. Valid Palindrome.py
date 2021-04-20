3a68388f42c6d36ff59f5ce44e2bcf30163b6189
shane <953762287@qq.com>
2021-04-20 13:42:12 +0800

    upd_125

---
+++ Week_10/125. Valid Palindrome.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        T：O(|s|)，其中 |s| 是字符串 s 的长度。
+        S：O(1)。
+        """
+        s = s.lower()
+        n = len(s)
+        l = 0
+        r = n - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l] != s[r]:
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+
+
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+
+        ss = ''.join(c for c in s.lower() if c.isalnum())
+        return ss[::-1] == ss
+
