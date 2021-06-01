d16daac55b0fe013456bdab332e35ad69e855961
shane <953762287@qq.com>
2021-06-01 19:57:34 +0800

    add_64

---
+++ Week_09/64. Minimum Path Sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+
+        T: O(mn)
+        S: O(mn)
+        """
+        if not grid or not grid[0]:
+            return 0
+        
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        # fist column, j = 0
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        
+        # fist row, i = 0
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+        
+        # other
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        
+        return dp[-1][-1]
