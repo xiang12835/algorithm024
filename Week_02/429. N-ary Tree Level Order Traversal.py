a6af3a2f8bcd949c213d4fb99944a18ed172443e
shane <953762287@qq.com>
2021-06-10 17:01:28 +0800

    upd_429

--- Week_02/429. N-ary Tree Level Order Traversal.py
+++ Week_02/429. N-ary Tree Level Order Traversal.py
@@ -28,3 +28,36 @@ class Solution:
                 queue.extend(node.children)
             res.append(cur)
         return res
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        """
+        双端队列
+
+        T: O(n)
+        S: O(n)
+        """
+        from collections import deque
+
+        if not root:
+            return
+        
+        res = []
+        queue = deque([root])
+        while queue:
+            cur = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                cur.append(node.val)
+                queue.extend(node.children)
+            res.append(cur)
+        return res
